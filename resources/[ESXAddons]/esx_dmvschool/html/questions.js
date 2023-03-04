c764faab43c3d48f29ee410218a81939baead1d7
DGserv <77808110+DGserv@users.noreply.github.com>
2023-03-04 12:27:33 +0100

    1.9.3 + Addons
    1.9.3 + Addons

---
+++ resources/[ESXAddons]/esx_dmvschool/html/questions.js
@@ -0,0 +1,91 @@
+var tableauQuestion = [
+	{
+		question: "Si vous roulez à 80 km/h et que vous vous approchez d'une zone résidentielle, vous devez :",
+		propositionA: "Vous accélérez",
+		propositionB: "Vous gardez votre vitesse, si vous ne dépassez pas d'autres véhicules",
+		propositionC: "Tu ralentis",
+		propositionD: "Tu gardes ta vitesse",
+		reponse: "C"
+	},
+
+	{
+		question: "Si vous tournez à droite à un feu de circulation, mais que vous voyez un passage pour piétons, que faites-vous :",
+		propositionA: "Vous passez avant le piéton",
+		propositionB: "Vous vérifiez qu'il n'y a pas d'autres véhicules autour",
+		propositionC: "Vous attendez que le piéton ait traversé",
+		propositionD: "Vous tirez sur le piéton et continuez à conduire",
+		reponse: "C"
+	},
+
+	{
+		question: "Sans indication préalable, la vitesse en zone résidentielle est de : __ km/h",
+		propositionA: "30 km/h",
+		propositionB: "50 km/h",
+		propositionC: "40 km/h",
+		propositionD: "60 km/h",
+		reponse: "B"
+	},
+
+	{
+		question: "Avant chaque changement de voie, vous devez :",
+		propositionA: "Vérifiez vos rétroviseurs",
+		propositionB: "Vérifiez vos angles morts",
+		propositionC: "Signalez vos intentions",
+		propositionD: "Tout ce qui précède",
+		reponse: "D"
+	},
+
+	{
+		question: "Quel taux d'alcoolémie est classé comme conduite en état d'ébriété ?",
+		propositionA: "0.05%",
+		propositionB: "0.18%",
+		propositionC: "0.08%",
+		propositionD: "0.06%",
+		reponse: "C"
+	},
+
+	{
+		question: "Quand pouvez-vous continuer à conduire à un feu rouge ?",
+		propositionA: "Quand c'est vert",
+		propositionB: "Quand il n'y a personne au carrefour",
+		propositionC: "Vous êtes en zone scolaire",
+		propositionD: "Quand c'est vert et/ou rouge et que tu tournes à droit",
+		reponse: "D"
+	},
+
+	{
+		question: "Un piéton a un signal de ne pas traverser, que faites-vous ?",
+		propositionA: "Tu les laisses passer",
+		propositionB: "Vous observez avant de continuer",
+		propositionC: "Tu fais signe pour leur dire de traverser",
+		propositionD: "Vous continuez car votre feu est vert",
+		reponse: "D"
+	},
+
+	{
+		question: "Ce qui est autorisé lors du dépassement d'un autre véhicule",
+		propositionA: "Vous le suivez de près pour le passer plus vite",
+		propositionB: "Tu le dépasses sans quitter la chaussée",
+		propositionC: "Vous roulez de l'autre côté de la route pour passer",
+		propositionD: "Vous dépassez la limite de vitesse pour les dépasser",
+		reponse: "C"
+	},
+
+	{
+		question: "Vous roulez sur une autoroute qui indique une vitesse maximale de 120 km/h. Mais la plupart des trafiquants roulent à 125 km/h, vous ne devez donc pas rouler plus vite que :",
+		propositionA: "120 km/h",
+		propositionB: "125 km/h",
+		propositionC: "130 km/h",
+		propositionD: "110 km/h",
+		reponse: "A"
+	},
+
+	{
+		question: "Lorsque vous êtes dépassé par un autre véhicule, il est important de NE PAS :",
+		propositionA: "Ralentir",
+		propositionB: "Vérifiez vos rétroviseurs",
+		propositionC: "Regarder les autres conducteurs",
+		propositionD: "Augmentez votre vitesse",
+		reponse: "D"
+	},
+]
---
+++ resources/[ESXAddons]/esx_hud/locales/en.lua
@@ -0,0 +1,15 @@
+Locales["en"] = {
+    toggleHud = 'Turn HUD on / off',
+    indicatorLeft = 'Left indicator',
+    indicatorRight = 'Right indicator',
+    toggleEngine = 'Engine on / off',
+    speedChanged = 'Speed changed, new speed: ',
+    resource_not_built = 'Resource are not built, please download the release version',
+    settingsSaveSuccess = "Successfully save current settings",
+    settingsResetSuccess = "Successfully reset settings",
+    currentVersion = "Latest version: ",
+    yourVersion = "Your version: ",
+    latestVersion = "You are use latest version!",
+    needUpdateResource = "You need download latest version! You are use old version at the moment!",
+    errorGetCurrentVersion = "You get error when try get current version!"
+}
